d9dbda013debb096bff5e61b20a9cd7f2ec1fe3e
Rafał Kuć <r.kuc@solr.pl>
2023-04-02 19:58:43 +0200

    Simple, dummy query in parser

--- solr/src/main/java/com/bb/solr/parser/BBuzzQueryParser.java
+++ solr/src/main/java/com/bb/solr/parser/BBuzzQueryParser.java
@@ -1,20 +1,26 @@
 package com.bb.solr.parser;
 
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.SyntaxError;
 
 public class BBuzzQueryParser extends QParser {
-  
   public BBuzzQueryParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
     super(qstr, localParams, params, req);
   }
-
-
   @Override
   public Query parse() throws SyntaxError {
-    return null;
+    // TODO: get synonyms and just run multiple queries
+    Query query = new BooleanQuery.Builder().
+        add(this.getQuery(), BooleanClause.Occur.SHOULD).
+        add(this.getQuery(), BooleanClause.Occur.SHOULD).
+        build();
+
+    return query;
   }
 }
